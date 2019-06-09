0fe39f83ba184ffedab8ff3a6f53bbfa114a316a
Vedad Bukva <vedadbukva8@gmail.com>
2019-06-09 22:47:56 +0200

    Implementirana klasa TelefonskiBroj

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class TelefonskiBroj {
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj>{
+
+    public abstract String ispisi();
+
+    public abstract int hashCode();
+
+    @Override
+    public int compareTo(TelefonskiBroj telefonskiBroj)
+    {
+        return this.ispisi().compareTo(telefonskiBroj.ispisi());
+    }
 }
