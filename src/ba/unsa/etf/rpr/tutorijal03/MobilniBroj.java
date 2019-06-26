c2a7c3390acd8c1cf4940fa6c22536c282ea2fab
Vedad Bukva <vedadbukva8@gmail.com>
2019-06-26 12:27:03 +0200

    Dodana klasa MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,6 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MobilniBroj {
-    public MobilniBroj(int i, String s) {
+public class MobilniBroj extends TelefonskiBroj{
+    private int mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) throws IllegalArgumentException {
+        if(mobilnaMreza>67 || mobilnaMreza<60)throw new IllegalArgumentException("Neispravna mreža!");
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String ispisi() {
+        return "0"+mobilnaMreza+"/"+broj;
     }
 }
